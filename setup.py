6c61439869e869428ca6a72ff3f847f1f6be7270
GregJeg2022 <96591023+GregJeg2022@users.noreply.github.com>
2026-04-27 10:06:34 -0400

    Bump version from 1.0.0 to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="JegBridge",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     python_requires=">=3.7",  # Minimum Python version
